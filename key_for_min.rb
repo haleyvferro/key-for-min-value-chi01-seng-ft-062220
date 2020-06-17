d47537090a0981ba967d034a4b341ef17b78f7bc
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 22:02:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,22 +12,3 @@ min_value = nil
   end
   min_key
 end
-
-  
-  #name_hash.each do |num|
- #   min = 1000
-  # if num < min
-   #  min = num
-#   end
-
-#  row_index = 0
-#  while row_index < src.count do
-#    element_index = 0
-#    while element_index < src[row_index].count do
-#      if src[row_index][element_index].even?
-#        p src[row_index][element_index]
-#      end
-#      element_index += 1
-#    end
-#    row_index += 1
-#  end
