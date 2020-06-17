93ba993f7368d870e2a88ed960731ed529951c2d
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 21:43:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
 min = 1000
-key = 
-  name_hash.each do |suite, name|
-    if numname.start_with?("A")
+min_key = 
+  name_hash.each do |key, value|
+    if 
       winner = name
     end
   end
