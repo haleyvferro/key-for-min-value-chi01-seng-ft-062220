60d48e60879e653d535c3f35a0376aa065f15389
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 20:52:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,31 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+min = 1000
+key = 
+  name_hash.each do |suite, name|
+    if numname.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
+   key_for_min_value
+  
+  #name_hash.each do |num|
+ #   min = 1000
+  # if num < min
+   #  min = num
+#   end
+
+#  row_index = 0
+#  while row_index < src.count do
+#    element_index = 0
+#    while element_index < src[row_index].count do
+#      if src[row_index][element_index].even?
+#        p src[row_index][element_index]
+#      end
+#      element_index += 1
+#    end
+#    row_index += 1
+#  end
